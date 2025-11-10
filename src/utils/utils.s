abf2214b1ac4982e8561cf3bed1223e51d293680
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2025-11-10 10:31:48 +0100

    Add NES Template
    Template from https://github.com/viktormendonck/nes_template

---
+++ src/utils/utils.s
@@ -0,0 +1,25 @@
+;*****************************************************************
+; ppu_update: waits until next NMI, turns rendering on (if not already), uploads OAM, palette, and nametable update to PPU
+;*****************************************************************
+.proc ppu_update
+	lda #1
+	sta nmi_ready
+	loop:
+		lda nmi_ready
+		bne loop
+	rts
+.endproc
+
+;*****************************************************************
+; ppu_off: waits until next NMI, turns rendering off (now safe to write PPU directly via PPU_DATA)
+;*****************************************************************
+.proc ppu_off
+	lda #2
+	sta nmi_ready
+	loop:
+		lda nmi_ready
+		bne loop
+	rts
+.endproc
+
+
