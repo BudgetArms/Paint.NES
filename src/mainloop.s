54095832fccba8dc2b8f9aff76e39f504fed5fba
Khine Lin Kyaw <khine.lin.kyaw@student.howest.be>
2025-11-23 14:42:25 +0100

    mainloop.s: Subroutine name change

--- src/mainloop.s
+++ src/mainloop.s
@@ -12,7 +12,7 @@ mainloop:
 
     jsr LoadCursor
     jsr UpdateCursorPosition
-    jsr convert_cursor_coordinates_to_cursor_tile_position
+    jsr ConvertCursorPosToTilePositions
     ;jsr LoadSmileyFace
 
 
