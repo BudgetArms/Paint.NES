da82a704e63c6d0a14915fe183fc3e3b860ddb8e
BudgetArms <67414205+BudgetArms@users.noreply.github.com>
2025-11-29 15:03:25 +0100

    Updated main: Fixed Cursor OAM position data
    Updated CURSOR_MEDIUM_DATA & CURSOR_BIG_DATA:
    -Due to the previously mentioned carry bit that was never cleared, there was an offset; it was not known what caused this, but the offset was counteracted with the OAM data.
    -Now that the carry bit is cleared before addition, the OAM data is changed so it doesn't contain the position offset.

--- src/main.s
+++ src/main.s
@@ -211,14 +211,14 @@ CURSOR_NORMAL_DATA:
 
 
 CURSOR_MEDIUM_DATA:
-    .byte   $07,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %10000000,     $00     ; BOTTOM-LEFT: mirrored y
+    .byte   $08,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %10000000,     $00     ; BOTTOM-LEFT: mirrored y
     .byte   $00,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %00000000,     $00     ; TOP-LEFT
     .byte   $00,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %01000000,     $08     ; TOP-RIGHT: mirrored x
     .byte   $08,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %11000000,     $08     ; BOTTOM-RIGHT: mirrored x & y
 
 
 CURSOR_BIG_DATA:
-    .byte   $0F,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %10000000,     $00     ; BOTTOM-LEFT: mirrored y
+    .byte   $10,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %10000000,     $00     ; BOTTOM-LEFT: mirrored y
     .byte   $08,  TILEINDEX_CURSOR_BIG_LEFT,       %00000000,     $00     ; LEFT
     .byte   $00,  TILEINDEX_CURSOR_BIG_TOP_LEFT,   %00000000,     $00     ; TOP-LEFT
     .byte   $00,  TILEINDEX_CURSOR_BIG_TOP,        %00000000,     $08     ; TOP
