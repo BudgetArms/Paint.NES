20a144dab0c1b87718069c826a3e59bdf3e4bcf0
Khine Lin Kyaw <khine.lin.kyaw@student.howest.be>
2025-12-05 20:13:03 +0100

    audio.s: New file added for audio subroutines
    A new file has been added to play audio related subroutines
    
    The audio playing part of DrawBrush subroutine has been moved to
    PlayBrushSoundEffect subroutine

---
+++ src/audio.s
@@ -0,0 +1,36 @@
+.proc PlayBrushSoundEffect
+
+    ; Play drawing sound effect here based on tool mode
+
+    lda selected_tool
+    cmp #PENCIL_TOOL_ACTIVATED
+    beq @Play_Splash
+    
+    ; Eraser mode - play bird sound (index 0)
+    lda #0
+    jmp @Play_Sound
+    
+    @Play_Splash:
+        ; Draw mode - play splash sound (index 1)
+        lda #1
+    
+    @Play_Sound:
+        ; Set the appropriate SFX channel based on which sound we're playing
+        ; Bird (0) uses square channel, Splash (1) uses noise channel
+        cmp #0
+        beq @use_square_channel
+        
+        ; Splash sound - use noise channel (SFX_CH1)
+        ldx #FAMISTUDIO_SFX_CH1
+        jmp @play_it
+        
+    @use_square_channel:
+        ; Bird sound - use square channel (SFX_CH0)
+        ldx #FAMISTUDIO_SFX_CH0
+        
+    @play_it:
+        jsr famistudio_sfx_play  ; Call FamiStudio directly with correct channel
+        lda #0
+    
+    rts
+.endproc
