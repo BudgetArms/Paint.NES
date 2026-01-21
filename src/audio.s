2bc525441633594281e774982df164e1815e56c1
Jeronimas Boots <161573100+Jeronimas-Boots@users.noreply.github.com>
2026-01-21 16:09:04 +0100

    sound channels

--- src/audio.s
+++ src/audio.s
@@ -54,6 +54,22 @@
     
     ; Default - no sound
     rts 
+
+
+
+    ; I would expect them to match the channels on hardware for example CH0 and CH1 would be square channels.
+    ; But when I try to match these channels to the ones used in Famistudio there is no sound and other wierd stuff happens :\
+    ; So the channels here might be incorrect but they sound correct and there are no other issues so I'll leave it at that.
+    ; Below is what is in the sound engine
+
+    ; .if FAMISTUDIO_CFG_SFX_SUPPORT
+    ;     FAMISTUDIO_SFX_STRUCT_SIZE = 15
+
+    ;     FAMISTUDIO_SFX_CH0 = FAMISTUDIO_SFX_STRUCT_SIZE * 0
+    ;     FAMISTUDIO_SFX_CH1 = FAMISTUDIO_SFX_STRUCT_SIZE * 1
+    ;     FAMISTUDIO_SFX_CH2 = FAMISTUDIO_SFX_STRUCT_SIZE * 2
+    ;     FAMISTUDIO_SFX_CH3 = FAMISTUDIO_SFX_STRUCT_SIZE * 3
+    ; .endif
     
     @Play_Shape:
         ; Shape tool - play shape sound (index 0) on both Square and Noise channels
@@ -79,19 +95,19 @@
         jmp @Play_SFX
 
     @Play_Fill:
-        ; Fill tool - play fill sound (index 2)
+        ; Fill tool - play fill sound (index 3)
         lda #$03
         ldx #FAMISTUDIO_SFX_CH1  ; Noise channel
         jmp @Play_SFX
         
     @Play_Draw:
-        ; Draw/Brush tool - play draw sound (index 3)
+        ; Draw/Brush tool - play draw sound (index 4)
         lda #$04
         ldx #FAMISTUDIO_SFX_CH1  ; Noise channel
         jmp @Play_SFX
 
     @Play_Text:
-        ; Text tool - play keypress DPCM sample
+        ; Text tool - play keypress DPCM sample (index 5)
         lda #$05
         jsr famistudio_sfx_sample_play
         
